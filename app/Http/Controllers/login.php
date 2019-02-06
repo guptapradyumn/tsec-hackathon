ecfffb2c79ed3aafa70fe6c74b5463a6aa12d23a
guptapradyumn <guptapradyumn284@gmail.com>
2019-02-06 18:21:15 +0530

    new3

--- app/Http/Controllers/login.php
+++ app/Http/Controllers/login.php
@@ -7,7 +7,39 @@ use Illuminate\Http\Request;
 class login extends Controller
 {
     //
+
     public function  auth(Request $Request){
+
+// $ipaddress = '';
+//         if (isset($_SERVER['HTTP_CLIENT_IP']))
+//             $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+//         else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+//             $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+//         else if(isset($_SERVER['HTTP_X_FORWARDED']))
+//             $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+//         else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+//             $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+//         else if(isset($_SERVER['HTTP_FORWARDED']))
+//             $ipaddress = $_SERVER['HTTP_FORWARDED'];
+//         else if(isset($_SERVER['REMOTE_ADDR']))
+//             $ipaddress = $_SERVER['REMOTE_ADDR'];
+//         else
+//             $ipaddress = 'UNKNOWN';
+//       return $ipaddress;
+ $PublicIP = '2405:204:30a:ad8f:b0eb:69da:3132:c2f3'; 
+ //  $output= $_SERVER['HTTP_REFERER'];
+ // return $output;
+
+ $json  = file_get_contents("http://ipinfo.io/$PublicIP/geo");
+ $json  =  json_decode($json ,true);
+ // $country =  $json['country'];
+ // $region= $json['region'];
+ // $city = $json['city'];
+ // return $city;
+
     	return view('home');
     }
+
+  
 }
+	
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,8 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Shop Homepage - Start Bootstrap Template</title>
+  <title>ecommerce</title>
+  
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -23,26 +24,24 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <a class="navbar-brand" href="#">Code Buddies</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
-            </a>
+            <input type="text" placeholder="type..."> &nbsp
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+           <button>Search</button>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
+           <li class="nav-item">
+            <a class="nav-link" href="#">Cart</a>
           </li>
-          <li class="nav-item">
+          <!--<li class="nav-item">
             <a class="nav-link" href="#">Contact</a>
-          </li>
+          </li> -->
         </ul>
       </div>
     </div>
